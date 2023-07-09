173e36232d91cc325c716607aba0f00d4047993d
Madhu788 <madhu.abekoon@gmail.com>
2023-07-09 12:41:25 +0100

    Angular User registration sample app

---
+++ AngularReg/src/app/registration/registration.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user.model';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../shared/user.service';
+
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit{
+  user: User = new User;
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.reset();
+    this.user = {
+      UserName: '',
+      Password: '',
+      Email: '',
+      FirstName: '',
+      LastName: ''
+    }
+  }
+  OnSubmit(form: NgForm) {
+    this.userService.registerUser(form.value)
+      .subscribe((data: any) => {
+        if (data.Succeeded == true) {
+          this.resetForm(form);
+          console.log('User registration successful');
+        }
+        else
+        console.log(data.Errors[0]);
+      });
+  }
+}
